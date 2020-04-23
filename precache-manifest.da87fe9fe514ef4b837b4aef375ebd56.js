d0c016bd9a06015e55383350e6a16c1b4dfc8216
freshkoala <zunigaruizb@gmail.com>
2020-04-23 15:18:10 +0800

    Updates

---
+++ precache-manifest.da87fe9fe514ef4b837b4aef375ebd56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "290256ac1007569aecdb",
+    "url": "/e-commerce/static/js/runtime~main.8575550b.js"
+  },
+  {
+    "revision": "84b4a700ec949d8654c3",
+    "url": "/e-commerce/static/js/main.0f93396c.chunk.js"
+  },
+  {
+    "revision": "17f04886fad594b2432d",
+    "url": "/e-commerce/static/js/2.9c71a620.chunk.js"
+  },
+  {
+    "revision": "84b4a700ec949d8654c3",
+    "url": "/e-commerce/static/css/main.1c4deeea.chunk.css"
+  },
+  {
+    "revision": "ed7707889637fd5f6e988f0cea9dfa5c",
+    "url": "/e-commerce/index.html"
+  }
+];
